c120113e94698627c927337fafb2af8554a17625
Takashi Nakagawa <tak1240@gmail.com>
2013-11-22 04:37:18 +0900

    rename my account name

---
+++ modules/people/manifests/takashi.pp
@@ -0,0 +1,23 @@
+# Class: people::tak0303
+#
+#
+class people::tak0303 {
+  # include your files...
+  include chrome::canary
+  include firefox
+  include mysql
+  include redis
+  include imagemagick
+  include alfred
+  include sequel_pro
+  include skype
+  include zsh
+  include ohmyzsh
+  include mongodb
+
+  package {
+    "Github for Mac":
+      source => "https://github-central.s3.amazonaws.com/mac/GitHub%20for%20Mac%20163.zip",
+      provider => "compressed_app";
+  }
+}
